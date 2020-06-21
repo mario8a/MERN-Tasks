e315989cf029aa53e9b7a1670f56245fe5ca24f8
Mario Ochoa <mario8ato@hotmail.com>
2020-06-21 00:05:16 -0500

    Trabajando con usuarios, server en express listo, jwt, cifrando password

--- servidor/index.js
+++ servidor/index.js
@@ -7,9 +7,14 @@ const app = express();
 //conectar a la DB
 conectarDB();
 
+//habilitar express.json, ayuda a leer datos que el usuario coloque
+app.use(express.json({extended:true}));
+
 //puerto de la app
 const PORT = process.env.PORT || 4000;
 
+//importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
 
 
 //arrancar la app
